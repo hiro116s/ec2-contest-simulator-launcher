a3afd3398c4aae762bb5c0cf2d56ae89c0e8b0bd
Hiroshi Nakahara <hiro116s@gmail.com>
2022-03-04 19:16:10 +0900

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.51.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "contest_simulation" {
+  launch_template {
+    name    = "contest-simulator-template"
+  }
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Learn-CloudInit"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.contest_simulation.public_ip
+}
