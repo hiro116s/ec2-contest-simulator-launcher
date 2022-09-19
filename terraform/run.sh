4f78a0337d07052b5cbd8753f7a983c9628efb10
Hiroshi Nakahara <hiro116s@gmail.com>
2022-09-19 18:51:24 +0900

    Fix

--- terraform/run.sh
+++ terraform/run.sh
@@ -2,8 +2,8 @@
 export PATH="$HOME/.cargo/bin:$PATH"
 
 cd /home/ubuntu/atcoder_library/src/main/java
-git pull
 git checkout ahc14
+git pull
 javac Main.java
 ./run.sh 1 # dry run
-java -cp /home/ubuntu/heuristics-contest-library-1.0-SNAPSHOT.jar hiro116s.simulator.MarathonCodeSimulator --commandTemplate './run.sh $SEED' --minSeed 0 --maxSeed 499 --s3 --contestName ahc14 --numThreads 10
+java -cp /home/ubuntu/heuristics-contest-library-1.0-SNAPSHOT.jar hiro116s.simulator.MarathonCodeSimulator --commandTemplate './run.sh $SEED' --minSeed 0 --maxSeed 499 --s3 --contestName ahc14 --numThreads 30
